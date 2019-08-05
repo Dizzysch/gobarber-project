73235bebdd0cbf3d783e0880dfdd4b42a06d8f35
Dizzysch <victorwilbert@gmail.com>
2019-08-05 07:17:18 -0300

    correção ortográfica

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -102,8 +102,9 @@ class AppointmentController {
     return res.json(appointment);
   }
 
-  async delete(req, res){
+  async delete(req, res) {
     return res.json();
   }
+}
 
 export default new AppointmentController();
