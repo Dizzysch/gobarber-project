216d5589d4b8933783549b6374aa24f3a2972594
Dizzysch <victorwilbert@gmail.com>
2019-08-12 17:14:20 -0300

    envia e-mail de cancelamento ao provider

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cdff3a16ba37d0',
+    pass: '4049b7bf161714',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
+
+// Mailtrap (DEV)
